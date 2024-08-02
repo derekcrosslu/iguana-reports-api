e47ec609c8e90c398dc355e7f5273cce22b96ff8
Derek Cross <derekcrosslu@gmail.com>
2024-08-01 19:04:23 -0500

    getting the current user
    getting the current user 2

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -25,21 +25,22 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
-  @Get('/colors/:color')
-  setColor(@Param('color') color: string, @Session() session: any) {
-    session.color = color;
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
   }
-  @Get('/colors')
-  getColor(@Session() session: any) {
-    return session.color;
-  }
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
   @Post('/signout')
   signout() {}
